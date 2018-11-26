b2a316a731c8058112ba5ef38f2f83bc09cb00bb
crystaljoy.tolentino@gmail.com <crystaljoy.tolentino@gmail.com>
2018-11-26 09:59:09 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -104,112 +104,13 @@ export class AppComponent {
     this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
   }
 
-  one() {
+  inputNum(input: number) {
     if (!this.isOperatorPressed && this.num1.length != 12) {
       this.isEqualsPressed = false;
-      this.num1.push(1);
+      this.num1.push(input);
       this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
     } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(1);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  two() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(2);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(2);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  three() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(3);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(3);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  four() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(4);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(4);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  five() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(5);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(5);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  six() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(6);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(6);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  seven() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(7);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(7);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  eight() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(8);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(8);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  nine() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(9);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(9);
-      this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
-    }
-  }
-
-  zero() {
-    if (!this.isOperatorPressed && this.num1.length != 12) {
-      this.isEqualsPressed = false;
-      this.num1.push(0);
-      this.num1Disp = parseInt(this.num1.toString().replace(/,/g, ""));
-    } else if (this.isOperatorPressed && this.num2.length != 12) {
-      this.num2.push(0);
+      this.num2.push(input);
       this.num2Disp = parseInt(this.num2.toString().replace(/,/g, ""));
     }
   }
